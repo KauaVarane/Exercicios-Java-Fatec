b6837ba5c45541e0d40661e7e1acf4d654cd4ec2
Kaua Santos Gomes Varane <kauavaranee@gmail.com>
2025-10-10 11:54:33 -0300

    Aluno_io
    Projeto Registro Lógico - Cadastro de Alunos em Java
    O sistema permite **cadastrar alunos**, **gravar as informações em um arquivo de texto**, e posteriormente **ler esses dados** para exibição.
    
    Funcionalidades:
    - Cadastro de Alunos:** solicita o nome, sobrenome e pontuação de cada aluno.
    - Gravação em Arquivo:** salva os registros no arquivo `ArquivoAluno.txt`.
    - Leitura de Dados:** lê e exibe os registros gravados anteriormente.
    -Estrutura em Classes:** código organizado com uso de métodos e objetos.

---
+++ Aluno.java
@@ -0,0 +1,16 @@
+package Aluno_io;
+class Aluno 
+{	 	 
+ String pnome;String unome;int    pontos;	 
+ Aluno ()
+ {	 
+  this("" ,"" ,0); 
+ }
+	 
+ Aluno ( String pnomealuno,String unomealuno,int pontosaluno )
+ {	 
+    pnome   = pnomealuno;	 
+    unome  = unomealuno;	 
+    pontos  = pontosaluno;	 
+ }	
+}	
